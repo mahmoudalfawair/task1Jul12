3a93515da1712b62d9ca5c2a7538d3d6a121bae5
mahmoud <mahmoud@mahmoud.home>
2021-07-13 00:00:08 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x ;
+    cin >> x ;
+    int y ;
+    cin >> y ;
+    if(x>y)
+        cout << x << endl;
+    else
+        cout << y << endl;
+    return 0;
+}
